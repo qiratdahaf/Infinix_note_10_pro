8d96d0c01709093859cd9c6d7f46e05d0ff97471
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2024-12-22 22:06:07 +0700

    Radiant: Update

--- bootctrl/BootControl.h
+++ bootctrl/BootControl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,8 +52,6 @@ class BootControl : public IBootControl {
     Return<bool> setSnapshotMergeStatus(MergeStatus status) override;
     Return<MergeStatus> getSnapshotMergeStatus() override;
 
-    // Methods from ::android::hardware::boot::V1_2::IBootControl follow.
-    Return<uint32_t> getActiveBootSlot() override;
   private:
     android::bootable::BootControl impl_;
     android::bootable::BootControlExt implext_;
